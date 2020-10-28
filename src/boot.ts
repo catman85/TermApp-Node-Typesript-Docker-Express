5f162611c986360ab7b315fffe6a67de39865dff
jimmy <jim@thinkpad.gr>
2020-10-29 01:36:02 +0200

    ext axios api implemented

--- src/boot.ts
+++ src/boot.ts
@@ -1,11 +1,7 @@
 import {
-  add
-} from './externalApi';
+  getDataPromise
+} from './externalApi'
 
-function speak(message: string) {
-  console.log(message);
-}
-
-speak('Ready fox! I\'m listening...');
-
-console.debug(add(5, 2));
+getDataPromise().then(res =>
+//   console.log(res.data)
+)
--- src/externalApi.ts
+++ src/externalApi.ts
@@ -1,3 +1,11 @@
-export const add = (x: number, y: number) => {
-  return x + y
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'https://corona.lmao.ninja/v2/countries?yesterday&sort',
+  headers: {}
+};
+
+export const getDataPromise= function (){
+  return axios(config)
 }
